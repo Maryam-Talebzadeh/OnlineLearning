6fd06417a52b9681855c1260047481b9f110ceba
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-02-24 05:00:50 -0800

    wrote niddle table between user and role.

---
+++ OnlineLearning.DataLayer/Entities/UserRole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.DataLayer.Entities
+{
+    public class UserRole
+    {
+
+        public UserRole()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+        #region Navigation Properties
+
+        public virtual User User { get; set; }
+        public virtual Role Role { get; set; }
+
+        #endregion
+    }
+}
