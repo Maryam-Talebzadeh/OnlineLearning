23bf2155571d9914fc0c0fcb8af70084f2d23114
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-02-24 05:19:43 -0800

    Wrote context with EfCore.

---
+++ OnlineLearning.DataLayer/Context/EfCore/OnlineLearningContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLearning.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.DataLayer.Context.EfCore
+{
+    public class OnlineLearningContext : DbContext
+    {
+        public OnlineLearningContext(DbContextOptions<OnlineLearningContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
