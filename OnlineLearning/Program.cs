072f23ce2542fb15334c5733d4f2bec6d331f706
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-02-24 06:00:53 -0800

    Introduced context to program.cs and installed the related packages.

--- OnlineLearning/Program.cs
+++ OnlineLearning/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OnlineLearning.DataLayer.Context.EfCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+#region DbContext
+
+builder.Services.AddDbContext<OnlineLearningContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineLearningConnectionString"));
+});
+
+#endregion
 
 var app = builder.Build();
 
