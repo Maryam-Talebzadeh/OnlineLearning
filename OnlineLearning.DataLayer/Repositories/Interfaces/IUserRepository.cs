2c38933c87632d5fabc3a04caa436cb9b62876ef
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-03-04 10:03:25 -0800

    Added IUserRepository and implement it to UserRepository.

--- OnlineLearning.DataLayer/Repositories/Interfaces/IUserRepository.cs
+++ OnlineLearning.DataLayer/Repositories/Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineLearning.DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace OnlineLearning.DataLayer.Repositories.Interfaces
 {
     public interface IUserRepository
     {
-         
+         public User AddUser(User user);
+        public bool UpdateUser(User user);
+        public bool DeleteUser(User user);
+        public User GetUserById(int id);
+        public List<User> GetAllUsers();
+        public bool LoginUser(string email, string password);
     }
 }
---
+++ OnlineLearning.DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,73 @@
+﻿using OnlineLearning.DataLayer.Context.EfCore;
+using OnlineLearning.DataLayer.Entities;
+using OnlineLearning.DataLayer.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.DataLayer.Repositories
+{
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly OnlineLearningContext _context;
+        public UserRepository(OnlineLearningContext context)
+        {
+            _context = context;
+        }
+
+        public User AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+        public bool DeleteUser(User user)
+        {
+            try
+            {
+                _context.Users.Remove(user);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _context.Users.SingleOrDefault(u => u.Id == id);
+        }
+
+        public bool LoginUser(string email, string password)
+        {
+
+            if (_context.Users.Any(u => u.Email == email && u.Password == password))
+                return true;
+
+            return false;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            try
+            {
+                _context.Users.Update(user);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
