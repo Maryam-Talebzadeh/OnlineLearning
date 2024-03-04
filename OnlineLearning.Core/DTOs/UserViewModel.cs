9ba9b7d62570f89cb50ea7a1903b6ee6594e7a9f
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-03-04 09:24:56 -0800

    I tried to create repository for user. So I created User ViewModel.

---
+++ OnlineLearning.Core/DTOs/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.Core.DTOs
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string UserAvatar { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
