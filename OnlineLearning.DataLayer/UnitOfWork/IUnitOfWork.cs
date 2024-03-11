5b26eb0db6067f7386fdee36643505e65c497268
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-03-11 05:45:13 -0700

    Implemented UnitOfWork.

---
+++ OnlineLearning.DataLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.DataLayer.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public void Save();
+    }
+}
---
+++ OnlineLearning.DataLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using OnlineLearning.DataLayer.Context.EfCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearning.DataLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OnlineLearningContext _db;
+
+        public UnitOfWork(OnlineLearningContext db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
